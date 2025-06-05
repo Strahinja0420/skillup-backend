f82e00bc76791fbb893f7179923e2ba822aec5fc
Strahinja420 <strahidelijica1@gmail.com>
2025-06-05 14:03:59 +0200

    forgot

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService)
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  
+  await app.listen(configService.get<number>('PORT') ?? 3000);
 }
 bootstrap();
